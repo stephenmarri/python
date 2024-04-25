11b63f3df41fa392281b2c90ba8c03c0e12fdd82
stephenm <stephenmarri7@gmail.com>
2024-04-25 16:44:50 -0500

    Add files via upload

---
+++ terrebone/v7/cls.py
@@ -0,0 +1,20 @@
+import ctypes
+import os.path
+from pathlib import Path
+
+
+def msg(message, msg_type):
+    if msg_type == "I":
+        ctypes.windll.user32.MessageBoxW(0, message, "Information", 1)
+    elif msg_type == "E":
+        ctypes.windll.user32.MessageBoxW(0, message, "Error", 1)
+
+
+def create_dir(dir_name):
+    if not os.path.exists(dir_name):
+        Path(dir_name).mkdir(parents=True, exist_ok=True)
+
+def crete_base_dirs():
+    lst = ["dir_output", "dir_download", "dir_intermediate", "dir_result"]
+    for dr in lst:
+        create_dir(dr)
