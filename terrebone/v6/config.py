57f6e65ce378f344c0177f4710266bb940eb1602
stephenm <stephenmarri7@gmail.com>
2024-04-25 14:34:00 -0500

    Add files via upload

---
+++ terrebone/v6/config.py
@@ -0,0 +1,6 @@
+# global values
+dir_output = 'D:/steve/random stuff/Terrebone/FHR/output/'
+dir_download = 'D:/steve/random stuff/Terrebone/FHR/download'
+dir_intermediate = 'D:/steve/random stuff/Terrebone/FHR/intermediate/'
+dir_result = 'D:/steve/random stuff/Terrebone/FHR/result/'
+file_input = 'D:/steve/random stuff/Terrebone/FHR/code/input.xlsx'
