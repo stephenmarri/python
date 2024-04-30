b981ebe03614ca7e1210d11c25dfba46e143a7f1
stephenm <stephenmarri7@gmail.com>
2024-04-30 18:07:54 -0500

    Add files via upload

---
+++ terrebone/code_charts/v1/curl_charts.py
@@ -0,0 +1,88 @@
+import csv
+import ctypes
+import datetime
+import os.path
+from pathlib import Path
+import requests
+
+
+def download_pdf(pat_name, ref_name, ind):
+    pat_output_dir = dir_charts_output + "/" + pat_name
+    create_dir(pat_output_dir)
+    out_ref_name = f"Chart_{ind}_" + process_file_name(ref_name)
+    out_path = pat_output_dir + '/' + out_ref_name
+    final_url = base_url + ref_name
+    curl(final_url, out_path)
+
+    row = [pat_name, pat_output_dir, ref_name, out_ref_name, out_path]
+    write_result(row)
+
+
+def curl(api_url, out_path):
+    global counter_success
+    cookies = {'PHPSESSID': 'pe06bhpqd98m22q8ficgg5cc8s', }
+    headers = {}
+    response = requests.get(api_url, cookies=cookies, headers=headers, )
+    with open(out_path, 'wb') as file_download:
+        file_download.write(response.content)
+    counter_success += 1
+
+
+def create_dir(dir_name):
+    if not os.path.exists(dir_name):
+        Path(dir_name).mkdir(parents=True, exist_ok=True)
+
+
+def get_lines_count(in_path) -> int:
+    lines_in_file = open(in_path, 'r').readlines()
+    number_of_lines = len(lines_in_file)
+    return number_of_lines
+
+
+def write_result(lst):
+    with open(output_report_path, 'a', newline='', encoding='utf-8') as file_namee:
+        writer = csv.writer(file_namee, dialect='excel')
+        writer.writerow(lst)
+
+
+def process_patient_name(patient_str) -> str:
+    patient_str = str(patient_str).split("__")[0]
+    return patient_str
+
+
+def process_file_name(file_str) -> str:
+    file_str = str(file_str)
+    if len(file_str) > 50 and file_str.endswith('.pdf'):
+        file_str = file_str[-59:]
+    elif len(file_str) > 25 and file_str.endswith('.pdf'):
+        file_str = file_str[-59:]
+    else:
+        file_str = ''
+    return file_str
+
+
+if __name__ == "__main__":
+    print("Process Started")
+    counter_success = 0
+
+    input_path = 'D:/steve/random stuff/Terrebone/FHR/charts/CollatedCharts_173443.csv'
+    root_dir = 'D:/steve/random stuff/Terrebone/FHR/charts/'
+    create_dir(root_dir)
+
+    base_url = 'https://ats.medtronsoftware.com/uploadedfile/ats/scanneddocs/'
+    root_time = str(datetime.datetime.now().strftime("%H%M%S"))
+    output_file_name = r"DownloadedCharts_" + root_time + ".csv"
+    output_report_path = root_dir + output_file_name
+    dir_charts_output = root_dir + 'charts_output_' + root_time
+    create_dir(dir_charts_output)
+
+    with open(input_path) as file:
+        counter_total_refs = get_lines_count(input_path)
+        reader = csv.reader(file, delimiter=",")
+        for i, line in enumerate(reader):
+            patient_name = process_patient_name(line[0])
+            pdf_name = line[1]
+            print(f"Processing Ref: [{i+1}/{counter_total_refs}] Success: [{counter_success}] \t Patient: [{patient_name}]")
+            download_pdf(patient_name, pdf_name, i+1)
+
+    ctypes.windll.user32.MessageBoxW(0, "Process Completed", "Information", 1)
---
+++ terrebone/code_charts/v1/get_charts.py
@@ -0,0 +1,32 @@
+import datetime
+import os.path
+import ctypes
+
+
+def write(str_input):
+    file_mod = open(output_path, "a")
+    file_mod.write(str_input + "\n")
+    file_mod.close()
+
+
+if __name__ == "__main__":
+    print("Process Started")
+    output_file_name = r"charts_" + datetime.datetime.now().strftime("%H%M%S") + ".txt"
+    output_path = 'D:/steve/random stuff/Terrebone/FHR/charts/' + output_file_name
+    root_folder = 'D:/steve/random stuff/Terrebone/FHR/download_completed'
+
+    if not os.path.exists(output_path):
+        f = open(output_path, "w")  # 'r' for reading and 'w' for writing
+        f.close()
+
+    counter = 1
+    success = 0
+    for root, dirs, files in os.walk(root_folder):
+        for file in files:
+            print(f"Scanning file: [{counter}]. Found files: [{success}]")
+            if str(file).endswith("ScannedCharts.txt"):
+                if os.path.getsize(os.path.join(root, file)) > 0:
+                    write(f"{root},{file}")
+                    success += 1
+
+    ctypes.windll.user32.MessageBoxW(0, "Process Completed", "Information", 1)
